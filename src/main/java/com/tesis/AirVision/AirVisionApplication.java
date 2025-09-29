ba2fd16cd36f06efbbeb3616c8d8ce07b2ade2a0
404873-Buscarini <404873@tecnicatura.frc.utn.edu.ar>
2025-09-28 23:50:51 -0300

    Basic Structure
    Signed-off-by: 404873-Buscarini <404873@tecnicatura.frc.utn.edu.ar>

---
+++ src/main/java/com/tesis/AirVision/AirVisionApplication.java
@@ -0,0 +1,13 @@
+package com.tesis.AirVision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirVisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tesis/AirVision/AirVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesis.AirVision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
