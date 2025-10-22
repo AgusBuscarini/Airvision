7a8117b038452c9e74545655d1971fce449a740e
404873-Buscarini <404873@tecnicatura.frc.utn.edu.ar>
2025-10-22 01:24:21 -0300

    JWT Implementado en backend
    Signed-off-by: 404873-Buscarini <404873@tecnicatura.frc.utn.edu.ar>

--- backend/src/test/java/com/tesis/AirVision/Controller/AuthControllerTest.java
+++ backend/src/test/java/com/tesis/AirVision/Controller/AuthControllerTest.java
@@ -3,6 +3,7 @@ package com.tesis.AirVision.Controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tesis.AirVision.Dtos.Login.LoginRequest;
 import com.tesis.AirVision.Dtos.Login.LoginResponse;
+import com.tesis.AirVision.Security.JwtService;
 import com.tesis.AirVision.Service.AuthService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,21 +32,28 @@ public class AuthControllerTest {
     @MockitoBean
     private AuthService authService;
 
+    @MockitoBean
+    private JwtService jwtService;
+
+    @MockitoBean
+    private UserDetailsService userDetailsService;
+
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     void Login_Success() throws Exception {
         LoginRequest loginRequest = new LoginRequest("test@test.com", "test1234");
-        LoginResponse loginResponse = new LoginResponse("Login exitoso", "USER");
+        LoginResponse loginResponse = new LoginResponse("Login exitoso", "USER", "token");
 
         when(authService.login(any(LoginRequest.class))).thenReturn(loginResponse);
 
-        mockMvc.perform(post("/auth/login")
+        mockMvc.perform(post("/api/auth/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(loginRequest)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value("Login exitoso"))
-                .andExpect(jsonPath("$.role").value("USER"));
+                .andExpect(jsonPath("$.role").value("USER"))
+                .andExpect(jsonPath("$.token").value("token"));
 
         verify(authService, times(1)).login(any(LoginRequest.class));
     }
@@ -52,11 +61,11 @@ public class AuthControllerTest {
     @Test
     void login_UserNotFound() throws Exception {
         LoginRequest request = new LoginRequest("notfound@test.com", "1234");
-        LoginResponse response = new LoginResponse("Usuario no encontrado", null);
+        LoginResponse response = new LoginResponse("Usuario no encontrado", null, null);
 
         when(authService.login(any(LoginRequest.class))).thenReturn(response);
 
-        mockMvc.perform(post("/auth/login")
+        mockMvc.perform(post("/api/auth/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isNotFound())
@@ -68,15 +77,16 @@ public class AuthControllerTest {
     @Test
     void login_InvalidCredentials() throws Exception {
         LoginRequest request = new LoginRequest("test@test.com", "wrong");
-        LoginResponse response = new LoginResponse("Credenciales inválidas", null);
+        LoginResponse response = new LoginResponse("Credenciales invalidas", null, null);
 
         when(authService.login(any(LoginRequest.class))).thenReturn(response);
 
-        mockMvc.perform(post("/auth/login")
+        mockMvc.perform(post("/api/auth/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isUnauthorized())
-                .andExpect(jsonPath("$.message").value("Credenciales inválidas"));
+                .andExpect(jsonPath("$.message").value("Credenciales invalidas"));
+
 
         verify(authService, times(1)).login(any(LoginRequest.class));
     }
