4b2a8a64173d119a16eaa356662314898a0632b0
404873-Buscarini <404873@tecnicatura.frc.utn.edu.ar>
2025-10-22 15:37:32 -0300

    Frontend actualizado con JWT, boton logout y zoom actualizado
    Signed-off-by: 404873-Buscarini <404873@tecnicatura.frc.utn.edu.ar>

--- frontend/app/map/page.tsx
+++ frontend/app/map/page.tsx
@@ -1,13 +1,35 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useEffect, useState, useRef } from "react"
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/authContext";
 
 const AirMap = dynamic(() => import("@/components/airmap"), { ssr: false });
 
-export default function HomePage() {
-  return (
-    <main className="h-screen w-screen">
-      <AirMap />
-    </main>
-  );
+export default function MapPage() {
+  const { isAuth, isLoading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading) {
+      if (!isAuth) {
+        router.push("/login");
+      }
+    }
+  }, [isAuth, isLoading, router]);
+
+  if (isLoading) {
+    return <div>Verificando sesión...</div>;
+  }
+
+  if (isAuth) {
+    return (
+      <main className="h-screen w-screen">
+        <AirMap />
+      </main>
+    );
+  }
+
+  return null;
 }
