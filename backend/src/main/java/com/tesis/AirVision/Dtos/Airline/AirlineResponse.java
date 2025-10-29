90c82d3be958b55f5d2a25776df866f19bf4676c
404873-Buscarini <404873@tecnicatura.frc.utn.edu.ar>
2025-10-29 00:35:51 -0300

    Endpoints de Gestion de Aerolineas con rol de ADMIN
    Signed-off-by: 404873-Buscarini <404873@tecnicatura.frc.utn.edu.ar>

---
+++ backend/src/main/java/com/tesis/AirVision/Dtos/Airline/AirlineResponse.java
@@ -0,0 +1,21 @@
+package com.tesis.AirVision.Dtos.Airline;
+
+import com.tesis.AirVision.Enums.Type;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class AirlineResponse {
+    private UUID id;
+    private String name;
+    private Type type;
+    private String iata;
+    private String icao;
+    private String countryCode;
+    private Boolean active;
+
+    private UUID ownerId;
+}
